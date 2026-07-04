ec090c27ce554e4773434b30845f9e6b4f942164
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2026-07-04 03:47:22 +0300

    update versions and controller

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -12,37 +12,31 @@ import org.springframework.web.client.RestTemplate;
 public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String baseURL = "http://node-service:5000/plusone";
 
-	RestTemplate restTemplate = new RestTemplate();
+	private static final String BASE_URL = "http://node-service:5000/plusone";
 
-	@RestController
-	public class compare {
+	private final RestTemplate restTemplate = new RestTemplate();
 
-		@GetMapping("/")
-		public String welcome() {
-			return "Kubernetes DevSecOps";
-		}
-
-		@GetMapping("/compare/{value}")
-		public String compareToFifty(@PathVariable int value) {
-			String message = "Could not determine comparison";
-			if (value > 50) {
-				message = "Greater than 50";
-			} else {
-				message = "Smaller than or equal to 50";
-			}
-			return message;
-		}
-
-		@GetMapping("/increment/{value}")
-		public int increment(@PathVariable int value) {
-			ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
-			String response = responseEntity.getBody();
-			logger.info("Value Received in Request - " + value);
-			logger.info("Node Service Response - " + response);
-			return Integer.parseInt(response);
-		}
+	@GetMapping("/")
+	public String welcome() {
+		return "Kubernetes DevSecOps";
 	}
 
+	@GetMapping("/compare/{value}")
+	public String compareToFifty(@PathVariable int value) {
+		if (value > 50) {
+			return "Greater than 50";
+		}
+		return "Smaller than or equal to 50";
+	}
+
+	@GetMapping("/increment/{value}")
+	public int increment(@PathVariable int value) {
+		ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL + "/" + value, String.class);
+
+		logger.info("Value Received in Request - {}", value);
+		logger.info("Node Service Response - {}", response.getBody());
+
+		return Integer.parseInt(response.getBody());
+	}
 }
